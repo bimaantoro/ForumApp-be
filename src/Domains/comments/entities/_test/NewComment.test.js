82683b4a32c3767f1028a4e2a37d252123e0df08
bimantoro <muhbimantoro08@gmail.com>
2024-07-13 00:04:49 +0800

    feat: newComment entities implemented with TDD

---
+++ src/Domains/comments/entities/_test/NewComment.test.js
@@ -0,0 +1,45 @@
+const NewComment = require('../NewComment');
+
+describe('a NewComment entities', () => {
+    it('should throw error when payload did not contain needed property', () => {
+        // Arrange
+        const payload = {
+          content: 'sebuah comment',
+          owner: 'user-123',
+        };
+    
+        // Action and Assert
+        expect(() => new NewComment(payload)).toThrowError('NEW_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY');
+      });
+
+      it('should throw error when payload did not meet data type specification', () => {
+        // Arrange
+        const payload = {
+          threadId: true,
+          content: 123,
+          owner: {},
+        };
+    
+        // Action and Assert
+        expect(() => new NewComment(payload)).toThrowError('NEW_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION');
+      });
+
+      it('should create thread object correctly', () => {
+        // Arrange
+        const payload = {
+          threadId: 'thread-123',
+          content: 'sebuah comment',
+          owner: 'user-123',
+        };
+    
+        // Action
+        const {
+          threadId, content, owner
+        } = new NewComment(payload);
+    
+        // Assert
+        expect(threadId).toEqual(payload.threadId);
+        expect(content).toEqual(payload.content);
+        expect(owner).toEqual(payload.owner);
+      });
+})
