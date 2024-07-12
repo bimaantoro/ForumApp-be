3a24144c5d0f48ed4ed09a9a0f3ce43bca4c5728
bimantoro <muhbimantoro08@gmail.com>
2024-07-12 18:06:21 +0800

    feat: exception Authentication, Authorization, Client, Invariant, NotFound implemented with TDD

---
+++ src/Commons/exceptions/_test/NotFoundError.test.js
@@ -0,0 +1,16 @@
+const ClientError = require('../ClientError');
+const NotFoundError = require('../NotFoundError');
+
+describe('NotFoundError', () => {
+  it('should create error correctly', () => {
+    const notFoundError = new NotFoundError('not found!');
+
+    expect(notFoundError).toBeInstanceOf(NotFoundError);
+    expect(notFoundError).toBeInstanceOf(ClientError);
+    expect(notFoundError).toBeInstanceOf(Error);
+
+    expect(notFoundError.statusCode).toEqual(404);
+    expect(notFoundError.message).toEqual('not found!');
+    expect(notFoundError.name).toEqual('NotFoundError');
+  });
+});
